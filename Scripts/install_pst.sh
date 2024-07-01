6dc39ae02cc64c960d386e1ee6e714e64e82d3a0
Jasper Concepcion <jasper@manok.dev>
2024-07-02 04:49:47 +0800

    feat: add personal config

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -78,3 +78,89 @@ if ! pkg_installed flatpak; then
 else
     echo -e "\033[0;33m[SKIP]\033[0m flatpak is already installed..."
 fi
+
+# nvm
+if pkg_installed nvm
+    then
+
+    echo "Adding nvm to .zshrc"
+    source /usr/share/nvm/init-nvm.sh
+    echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
+
+    if command -v node &> /dev/null
+        then
+        echo "Node already installed, skipping installation..."
+    else
+        echo "Installing node"
+        nvm install 18
+        nvm use 18
+    fi
+
+    if command -v pnpm &> /dev/null
+        then
+        echo "PNPM already installed, skipping installation..."
+    else 
+      echo "Installing PNPM"
+      curl -fsSL https://get.pnpm.io/install.sh | sh -
+    fi
+
+else
+    echo "WARNING: nvm, node, and pnpm not installed..."
+fi
+
+if command -v tmux &> /dev/null
+    then
+    echo "Installing TMUX Plugin Manager"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || echo "Skipping TMUX Plugin Manager Installation"
+fi
+
+
+if pkg_installed fzf 
+    then
+    echo "Installing fzf-git.sh"
+
+    if [ ! -d ~/Scripts ]; then 
+      echo "Creating ~/Scripts folder"
+      mkdir ~/Scripts
+    fi
+
+    curl "https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh" > ~/Scripts/fzf-git.sh
+    echo "source ~/Scripts/fzf-git.sh" >> ~/.zshrc
+fi
+
+if pkg_installed bob 
+    then
+    echo "Installing neovim nightly using bob"
+    bob use nightly
+
+    echo "Setting up neovim config"
+    rm -rf ~/.config/nvim-manokii
+    rm -rf ~/.local/share/nvim-manokii
+    git clone https://github.com/manokii/nvim ~/.config/nvim-manokii
+    echo "alias vim='NVIM_APPNAME=nvim-manokii nvim'" >> ~/.zshrc
+
+fi
+
+if pkg_installed git-delta 
+    then
+    
+    echo "Updating git config for delta"
+    git config --global core.pager "delta"
+    git config --global interactive.diffFilter "delta --color-only"
+    git config --global delta.navigate "true"
+    git config --global delta.line-numbers "true"
+    git config --global merge.conflictstyle "diff3"
+    git config --global diff.colorMoved "default"
+fi
+
+
+if pkg_installed bat
+    then
+
+    echo "Setting up bat theme (tokyo night)"
+    batDir="$(bat --config-dir)/themes"
+    mkdir -p "$batDir"
+    curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme > "$batDir/tokyonight_night.tmTheme"
+    bat cache --build
+fi
+
